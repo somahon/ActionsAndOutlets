39b5e3fa74c470f7ee1132d174d9ed2e70a61739
Shane O'Mahony <sleepymole@users.noreply.github.com>
2018-10-20 14:39:30 +0100

    Initial Commit

---
+++ ActionsAndOutlets/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ActionsAndOutlets
+//
+//  Created by Shane O'Mahony on 20/10/2018.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
